36d2a4c90a4538e9d87af5e21a0abcce7263280e
abdelrahmanShaaban <boodyelmasry6@gmail.com>
2024-02-21 13:40:44 +0200

    start initilize application and run

--- company/models.py
+++ company/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
 # Create your models here.
+class Empolyee(models.Model):
+    name=models.CharField(max_length=50)
+    salary=models.models.IntegerField()
+    title=models.CharField(max_length=50)
