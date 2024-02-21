420e83d1e832b7ff6f1d3e5ce072d92198ffa30d
abdelrahmanShaaban <boodyelmasry6@gmail.com>
2024-02-21 14:06:11 +0200

    create forgin key for them

--- company/models.py
+++ company/models.py
@@ -5,8 +5,10 @@ class Empolyee(models.Model):
     name=models.CharField(max_length=50)
     salary=models.IntegerField()
     title=models.CharField(max_length=50)
+    team=models.ForeignKey("Team" ,on_delete=models.CASCADE,null=True,blank=True)
 
 
 class Team(models.Model):
     name=models.CharField(max_length=50)
-    manager=models.ForeignKey(Empolyee,on_delete=models.PROTECT)
+    manager=models.ForeignKey(Empolyee,on_delete=models.PROTECT,related_name="team_manager")
+
