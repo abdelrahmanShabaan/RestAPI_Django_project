c5548dd36dc0b0f45308a767f81926ca39e682dd
abdelrahmanShaaban <boodyelmasry6@gmail.com>
2024-02-21 14:01:05 +0200

    make to model for empolye and team

--- company/models.py
+++ company/models.py
@@ -3,5 +3,10 @@ from django.db import models
 # Create your models here.
 class Empolyee(models.Model):
     name=models.CharField(max_length=50)
-    salary=models.models.IntegerField()
+    salary=models.IntegerField()
     title=models.CharField(max_length=50)
+
+
+class Team(models.Model):
+    name=models.CharField(max_length=50)
+    manager=models.ForeignKey(Empolyee,on_delete=models.PROTECT)
